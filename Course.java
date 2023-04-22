e51364043c399a6db2c55707378a3a5c31071f89
AidenRWise <aiwise@ucsd.edu>
2023-04-22 00:58:04 -0700

    Course Class Setup

--- Course.java
+++ Course.java
@@ -1,13 +1,45 @@
 public class Course {
-    String name;
-    String id;
-    double startTime;
-    double endTime;
+    private String name;
+    private String id;
+    private double startTime;
+    private double endTime;
 
     public Course(String name, String id, double startTime, double endTime){
         this.name = name;
         this.id = id;
         this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public double getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(double startTime) {
+        this.startTime = startTime;
+    }
+
+    public double getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(double endTime) {
         this.endTime = endTime;
     }
 }
