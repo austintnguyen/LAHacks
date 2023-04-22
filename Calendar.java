aca4116474c5eade820b175d2518c2770467498c
AidenRWise <aiwise@ucsd.edu>
2023-04-22 02:06:37 -0700

    Update Calendar

--- Calendar.java
+++ Calendar.java
@@ -1,15 +1,25 @@
 
-public class Calendar{
+public class Calendar {
 
-    private ArrayList<ArrayList<Course>> list;
+    private ArrayList[] list;
+
     public Calendar() {
-        list = new ArrayList<ArrayList<Course>>();
+        for (int i = 0; i < 5; i++) {
+            list[i] = new ArrayList<Course>();
+        }
     }
 
-    public void inputCalendar(Person person){
+    public void inputCalendar(Person person) {
 
-        
+        for (int i = 0; i < person.getList().size(); i++) {
+            for (int j = 0; j < 5; j++) {
+
+                if (person.getList().get(i).getDays(j)) {
+                    list[j].add(person.getList().get(i));
+                }
+
+            }
+        }
 
     }
-
 }
