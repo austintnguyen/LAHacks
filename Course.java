5b4ab8b713e9601ca2937613a9783a689e64ca89
austintnguyen <atn038@ucsd.edu>
2023-04-22 00:45:34 -0700

    Framework

---
+++ Course.java
@@ -0,0 +1,13 @@
+public class Course {
+    String name;
+    String id;
+    double startTime;
+    double endTime;
+
+    public Course(String name, String id, double startTime, double endTime){
+        this.name = name;
+        this.id = id;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+}
---
+++ Person.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Person {
+
+String name;
+ArrayList<Course> list;
+
+
+public Person(){
+name = "";
+list = null;
+}
+
+public Person(String name, ArrayList<Course>list){
+
+this.name = name;
+this.list = list;
+
+}
+
+
+
+}
