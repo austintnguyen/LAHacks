d4c1eb7dcb048e0bea934ce23d9e036090bb5c6c
austinyu12 <ausno5@gmail.com>
2023-04-22 10:04:13 -0700

    course

--- Person.java
+++ Person.java
@@ -2,44 +2,36 @@ import java.util.*;
 
 public class Person {
 
-private String name;
-private ArrayList<Course> list;
+    private String name;
+    private ArrayList<Course> list;
 
+    public Person(String name, ArrayList<Course> list) {
+        this.name = name;
+        this.list = list;
+    }
 
+    public void addCourse(Course course) {
+        list.add(course);
+    }
 
-public Person(String name, ArrayList<Course>list){
-
-this.name = name;
-this.list = list;
-
-}
-
-public void addCourse(Course course){
-
-list.add(course);
-
-}
-
-public void removeCourse(String name){
-    for(int i = 0; i<list.size();i++){
-        if(list.get(i).name == name){
-            list.remove(i);
+    public void removeCourse(String name) {
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).name == name) {
+                list.remove(i);
+            }
         }
-}
+    }
 
-}
+    public String getName() {
+        return name;
+    }
 
-public String getName(){
-return name;
-}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-public void setName(String name){
-this.name = name;
-}
-
-public ArrayList<Course> getList(){
-    return list;
-}
-
+    public ArrayList<Course> getList() {
+        return list;
+    }
 
 }
